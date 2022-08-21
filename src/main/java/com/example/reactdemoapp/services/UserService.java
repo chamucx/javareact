438d307e4d4ecc04153f3f9451cc5830161fcc7e
Antonio <joseantoniosysnet@gmail.com>
2022-08-21 22:32:22 +0200

    added users

--- src/main/java/com/example/reactdemoapp/services/UserService.java
+++ src/main/java/com/example/reactdemoapp/services/UserService.java
@@ -1,12 +1,33 @@
 package com.example.reactdemoapp.services;
 
+import com.example.reactdemoapp.UserRepository;
+import com.example.reactdemoapp.entities.UserEntity;
 import com.example.reactdemoapp.models.shared.dtos.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
-public class UserService implements UserServiceInterface{
+public class UserService implements UserServiceInterface {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("testPass");
+        userEntity.setUserId("IDpblico");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto userToReturn = new UserDto();
+        BeanUtils.copyProperties(userEntity, userToReturn);
+
+        return userToReturn;
     }
 }
